c72ad6e980a791809d1c7d6fc5f7a1a942be3ce9
Rahmad Firdaus <163318781+rhmdfrds1212@users.noreply.github.com>
2025-06-23 22:11:03 +0700

    up

--- Advertising/database/migrations/2025_05_29_073919_create_pembayarans_table.php
+++ Advertising/database/migrations/2025_05_29_073919_create_pembayarans_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pembayarans', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary();
             $table->foreignUuId('pemesanan_id')->constrained()->onDelete('cascade');
             $table->enum('metode', ['transfer_bank', 'qris', 'cod']);
             $table->string('status')->default('menunggu pembayaran');
